b25e41a160a5cd49c0415b0e23c41834e5547345
Krzysztof Knapik <knapo@knapo.net>
2023-12-11 13:29:30 +0100

    fix pulling images on different branches

--- lib/vidar/cli.rb
+++ lib/vidar/cli.rb
@@ -22,9 +22,11 @@ module Vidar
     desc "pull", "Pull existing docker images to leverage docker caching"
     def pull
       Log.info "Pulling #{Config.get!(:image)} tags"
-      Run.docker "pull #{Config.get!(:image)}:#{Config.get!(:base_stage_name)}-#{Config.get!(:current_branch)} 2> /dev/null || true"
-      unless Config.default_branch?
+      if Config.default_branch?
+        Run.docker "pull #{Config.get!(:image)}:#{Config.get!(:base_stage_name)}} 2> /dev/null || true"
         Run.docker "pull #{Config.get!(:image)}:#{Config.get!(:base_stage_name)}-#{Config.get!(:default_branch)} 2> /dev/null || true"
+      else
+        Run.docker "pull #{Config.get!(:image)}:#{Config.get!(:base_stage_name)}-#{Config.get!(:current_branch)} 2> /dev/null || true"
       end
       Log.info "Docker images:"
       Run.docker("images")
